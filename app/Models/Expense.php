dce4f7de78ad748a841f21edb3eccaa09bbda60e
sajedul559 <sajedulkhairul@gmail.com>
2024-12-23 21:56:56 +0600

    expense crud done

---
+++ app/Models/Expense.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'vendor_id',
+        'date',
+        'property_id',
+        'created_by',
+        'last_updated_by',
+        'last_update_date',
+        'expense_type',
+        'amount',
+        'is_credit',
+    ];
+
+    public const CLEANING_COST = 'Cleaning Cost';
+    public const ELECTRICITY_BILL = 'Electricity Bill';
+    public const WATER_BILL = 'Water Bill';
+
+
+    // Return property types as an associative array
+    public static function getExpenseTypes(): array
+    {
+        return [
+            self::CLEANING_COST => 'Cleaning Cost',
+            self::ELECTRICITY_BILL => 'Electricity Bill',
+            self::WATER_BILL => 'Water Bill',
+        ];
+    }
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
+
+    public function property()
+    {
+        return $this->belongsTo(Property::class);
+    }
+
+}
---
+++ database/migrations/2024_12_23_192831_create_expenses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vendor_id');
+            $table->date('date')->nullable();
+            $table->unsignedBigInteger('property_id')->nullable();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('last_updated_by')->nullable();
+            $table->string('expense_type')->nullable();
+            $table->decimal('amount', 15, 2);
+            $table->boolean('is_credit')->default(false);
+            $table->timestamp('last_update_date')->nullable();
+            $table->timestamps();
+
+            $table->foreign('vendor_id')->references('id')->on('vendors')->onDelete('cascade');
+            $table->foreign('property_id')->references('id')->on('properties')->onDelete('set null');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('last_updated_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
