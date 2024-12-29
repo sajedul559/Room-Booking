a536c54d1340a0df8dba4f4cb9f92bbd6e4a1477
sajedul559 <sajedulkhairul@gmail.com>
2024-12-29 22:05:56 +0600

    add vite and vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Welcome to Inertia with Vue.js</h1>
+        <p>This is an example page.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+};
+</script>
+
+<style>
+/* Optional: Add your styles here */
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
+import vue from "@vitejs/plugin-vue";
+
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: [
                 'resources/sass/app.scss',
