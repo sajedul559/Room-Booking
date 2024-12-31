a0cbeb133f8d9e7932b5cf402480043c56ada2f4
sajedul559 <sajedulkhairul@gmail.com>
2024-12-31 15:03:45 +0600

    working on theme integrate

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel with Inertia</title>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
+    <link rel="stylesheet" href="{{ asset('assets/frontend/css/style.css') }}">
+
 </head>
 <body>
     @inertia
