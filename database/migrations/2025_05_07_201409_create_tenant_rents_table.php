04a1ccf9a7aa59eaba7287a68b6c36f4ea08319f
Farhankaioum <keiuom.swe@gmail.com>
2025-05-08 00:38:40 +1000

    TenantRent entity added with migration

---
+++ database/migrations/2025_05_07_201409_create_tenant_rents_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tenant_rents', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('amount', 10, 2);
+            $table->decimal('paid_amount', 10, 2)->default(0);
+            $table->enum('status', ['pending', 'partial', 'paid'])->default('pending');
+            $table->date('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenant_rents');
+    }
+};
